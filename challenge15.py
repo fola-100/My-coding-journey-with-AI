1ead00f24e6a4632bbaf77eb02568e27a9479e28
OLAMIDE <ofolaranmi068@gmail.com>
2026-01-15 21:31:00 +0100

    Account Registration System (CLI)(SECOND-ATTEMPT)

--- challenge15.py
+++ challenge15.py
@@ -138,3 +138,95 @@ respond_vault={}
 #Combines results
 #Returns different shapes (list, tuple, dict)
 
+#SECOND-ATTEMPT
+
+#VALIDATING NAME
+def validate_username(user_name):
+    name_respond=[]
+    #CHECKING IF CONDITION WAS MET
+    only_letter= all(char.isalpha() for char in user_name)
+    character_len= len(user_name)>=3
+    space_condition=not any(char.isspace() for char in user_name)
+    contain_value= user_name!=""
+    container={"letter":"Ensure name only contain a letter",
+               "length":"Ensure name contains more than three characters",
+               "space":"Ensure name doesn't contain any space",
+               "item":"Ensure you enter in a name"
+               }
+    check_lists=[only_letter,character_len,space_condition,contain_value]
+    condition =all(check for check in check_lists)
+    #RESPONES IF CONDITION IS NOT MET
+    if not condition:
+        if not contain_value:
+            name_respond.append(container["item"])
+        else:
+            if not only_letter:
+              name_respond.append(container["letter"])
+            if not character_len:
+               name_respond.append(container["length"])
+            if not space_condition:
+              name_respond.append(container["space"])
+    # RETURNING RESPONSES SAVED IN LIST
+        return name_respond
+    else:
+        name_respond.append(user_name)
+        return name_respond
+
+
+#VALIDATING AGE
+def validate_age(user_age):
+    age_vault=[]
+    #CHECKING IF VALUE WAS GIVING
+    if not user_age:
+        age_vault.append("Ensure you enter age value")
+        return age_vault
+    # CHECKING IF VALUE MEET CONDITION AND RESPONSE FOR ERROR
+    try:
+        age=int(user_age)
+        if 13<=age<=120:
+            age_vault.append(age)
+            return age_vault
+        else:
+            age_vault.append("Ensure age is between the range of 13-120")
+        return age_vault
+
+    except ValueError:
+         age_vault.append( "Ensure age entered is a whole number")
+         return age_vault
+#REGISTER USER
+def register_user(name,age):
+    respond_vault={}
+    #RESPONSE IF NO VALUE WAS GIVING
+    if not name:
+         respond_vault["name_error"]=["Ensure you enter a name"]
+    if not age:
+        respond_vault["age_error"]=["Ensure you enter a age"]
+    if respond_vault:
+        return respond_vault
+    #SAVING RESPONSE AND RETURN VALUE
+    name_response=validate_username(name)
+    age_response=validate_age(age)
+    respond_vault["name"]= name_response
+    respond_vault["age"]=age_response
+
+    return respond_vault
+
+test=input("enter one option:")
+if test=="1":
+    # NAME VALIDATION
+ respond_value=validate_username(input("Enter a name:"))
+ for respond in respond_value:
+     print(respond)
+
+if test=="2":
+#AGE VALIDATION
+ respond_value=validate_age(input("Enter your age:"))
+ for respond in respond_value:
+     print(respond)
+
+if test=="3":
+    return_value=register_user(name=input("Enter your name:"),age=input("Enter your age:"))
+    for key,value in return_value.items():
+        print(value)
+
+
