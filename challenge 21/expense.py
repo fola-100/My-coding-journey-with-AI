e8bbdeb23281f1c1f956c50445662f98343cd1ed
fola-100 <ofolaranmi097@gmail.com>
2026-03-12 20:42:12 +0100

    ExpenseObject

---
+++ challenge 21/expense.py
@@ -0,0 +1,6 @@
+class ExpenseObject:
+    def __init__(self,amount,category,description,data):
+        self.amount=amount
+        self.category=category
+        self.description=description
+        self.data=data
